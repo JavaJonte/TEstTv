7b8977d84b56643202bf6562e295ea309adba54e
Jonathan Wikman <73289628+JavaJonte@users.noreply.github.com>
2021-04-09 14:56:02 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import SideMenu from './SideMenu';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+        <i class="fas fa-bars menu-test"></i>
+      </div>
+      
+      <Col>
+        SVT 1
+</Col>
+      <ul class="program-list">
+        <li class="program-list__item">
+          13:00<br />
+    Vinterstudion
+  </li>
+        <li class="program-list__item">
+          13:30<br />
+    Alpint: VM
+  </li>
+        <li class="program-list__item">
+          14:30<br />
+    Vinterstudion
+  </li>
+      </ul>
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/SideMenu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SideMenu() {
+    return (
+        <div class="menu">
+        <a class="menu__link">SVT 1</a>
+        <a class="menu__link">SVT 2</a>
+        <a class="menu__link">SVT Barn</a>
+        <a class="menu__link">Kunskapskanalen</a>
+        <a class="menu__link">SVT 24</a>
+      </div>
+    )
+}
