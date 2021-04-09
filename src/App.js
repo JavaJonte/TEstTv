c5d037e46376dfec2e45d80ba7b42f1f5e9e6e94
Jonathan Wikman <73289628+JavaJonte@users.noreply.github.com>
2021-04-09 22:49:41 +0200

    lekt med programlistan plus lite css

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import SideMenu from './SideMenu';
+import ProgramList from './ProgramList'
 import { Container, Row, Col } from 'react-bootstrap';
 
 
 function App() {
   return (
-    <Container>
-      <div>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-        <i class="fas fa-bars menu-test"></i>
-      </div>
-      
-      <Col>
-        SVT 1
-</Col>
-      <ul class="program-list">
-        <li class="program-list__item">
-          13:00<br />
-    Vinterstudion
-  </li>
-        <li class="program-list__item">
-          13:30<br />
-    Alpint: VM
-  </li>
-        <li class="program-list__item">
-          14:30<br />
-    Vinterstudion
-  </li>
-      </ul>
-    </Container>
+    <>
+
+      <Container>
+        <div>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+          <i class="fas fa-bars menu-test"></i>
+          <h1 class="channel-title">SVT 1</h1>
+          <SideMenu />
+          <ProgramList />
+        </div>
+
+      </Container>
+
+    </>
   )
 }
 
---
+++ src/ProgramList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function ProgramList() {
+    return (
+        <div class="programs">
+            <ul class="program-list">
+                <li class="program-list__item">
+                    13:00<br />
+    Vinterstudion
+  </li>
+                <li class="program-list__item">
+                    13:30<br />
+    Alpint: VM
+  </li>
+                <li class="program-list__item">
+                    14:30<br />
+    Vinterstudion
+  </li>
+            </ul>
+        </div>
+    )
+}
